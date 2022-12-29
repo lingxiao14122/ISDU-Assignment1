e1ca065ff9bcb93364ba421ef233e15d70643e65
Ling Xiao Chok <ling-xiao.chok.wg@renesas.com>
2022-12-29 16:55:19 +0800

    sidebar links

---
+++ Assignment1/Pages/Department/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Assignment1.Pages.Departments
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
