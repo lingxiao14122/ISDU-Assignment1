45cd248008fed1bf5b6520c07214ae3776bf1e4f
Ling Xiao Chok <ling-xiao.chok.wg@renesas.com>
2022-12-30 16:03:42 +0800

    Read user (page)

--- Assignment1/Pages/Users/Index.cshtml.cs
+++ Assignment1/Pages/Users/Index.cshtml.cs
@@ -1,12 +1,63 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+using static Assignment1.Pages.Users.IndexModel;
 
 namespace Assignment1.Pages.Users
 {
     public class IndexModel : PageModel
     {
+        public List<User> listUsers = new List<User>();
         public void OnGet()
         {
+            try
+            {
+                String connectionString = "Data Source=.\\sqlexpress;Initial Catalog=assignment;Integrated Security=True";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    String sql = "SELECT * FROM Users";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                User user = new User();
+                                user.id = "" + reader.GetInt32(0);
+                                user.user_name = reader.GetString(1);
+                                user.email = reader.GetString(2);
+                                user.employee_number = reader.GetString(3);
+                                user.age = "" + reader.GetInt32(4);
+                                // 5 password
+                                user.dept_id = "" + reader.GetInt32(6);
+                                user.active = "" + reader.GetBoolean(7);
+                                user.created_at = reader.GetDateTime(8).ToString();
+
+                                listUsers.Add(user);
+                                Console.WriteLine(user.id);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.ToString());
+            }
         }
     }
+    public class User
+    {
+        public String id;
+        public String user_name;
+        public String email;
+        public String employee_number;
+        public String age;
+        public String dept_id;
+        public String active;
+        public String created_at;
+    }
+
+
 }
